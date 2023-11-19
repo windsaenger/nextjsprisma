cb09d2e94c2180c3866eec43bf70bef066028369
jdcundle <jdcundle@live.com>
2023-11-19 13:41:07 -0500

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: isProd ? '' : '',
+    basePath: '',
     //output: 'export',
    
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
